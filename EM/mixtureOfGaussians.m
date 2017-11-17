5e438cfb293ff97e373bcf137380019910fb8489
Brian Jackson <bjack205@gmail.com>
2017-11-17 10:48:35 -0800

    EM Algorithm

---
+++ EM/mixtureOfGaussians.m
@@ -0,0 +1,77 @@
+% Taylor Howell
+% EM algorithm to fit mixture of Gaussians
+% 11-14-2017
+
+clear;clc;close all;
+n = 2; % dimension of each training example
+k = 4; % number of clusters
+
+%% Generate k 2-dimensional Gaussians and sample from them to create training examples
+examples = 100;
+mu_initial = [];
+sigma_initial = [];
+Xi = [];
+X = [];
+for i = 1:k
+    mu_initial{i} = 5*randn(1,n);
+    %tmp = rand(n,n);
+    sigma_initial{i} = rand(1)*eye(n);%tmp'*tmp;
+    Xi{i} = randn(examples, n) * chol(sigma_initial{i}) + repmat(mu_initial{i}, examples, 1);
+    X = cat(1,X,Xi{i});
+end
+
+%% Run EM
+[mu, sigma] = EM_mix_gauss(X,k,.001);
+
+%% Visualize Mixture of Gaussians
+gridSize = 100;
+u = linspace(-20, 20, gridSize);
+[A B] = meshgrid(u, u);
+gridX = [A(:), B(:)];
+
+figure
+subplot(1,2,1)
+hold on;
+for j = 1:k
+    % plot raw data
+    plot(Xi{j}(:, 1), Xi{j}(:, 2), 'o');
+  
+    % plot contour from know distributions
+    z = pdf_gaussian_multi(gridX, mu_initial{j}, sigma_initial{j});
+    contour(u, u, reshape(z, gridSize, gridSize),'k');
+end
+title('Fit from know distributions')
+axis square
+
+subplot(1,2,2)
+hold on;
+for j = 1:k
+    % plot raw data
+    plot(Xi{j}(:, 1), Xi{j}(:, 2), 'o');
+end
+for j = 1:k
+    % plot contour from know distributions
+    z = pdf_gaussian_multi(gridX,mu(j,:), sigma{j});
+    contour(u, u, reshape(z, gridSize, gridSize),'k');
+end
+title('Mixture of Gaussians')
+axis square
+
+%% Run EM multiple times on same training set
+figure
+for i = 1:4
+    [mu, sigma] = EM_mix_gauss(X,k,.00001);
+    subplot(2,2,i)
+    hold on;
+    for j = 1:k
+        % plot raw data
+        plot(Xi{j}(:, 1), Xi{j}(:, 2), 'o');
+    end
+    for j = 1:k
+        % plot contour from know distributions
+        z = pdf_gaussian_multi(gridX,mu(j,:), sigma{j});
+        contour(u, u, reshape(z, gridSize, gridSize),'k');
+    end
+    title('Different EM initializations')
+    axis square 
+end
---
+++ EM/pdf_gaussian_multi.m
@@ -0,0 +1,10 @@
+function [ p ] = pdf_gaussian_multi(X,mu,sigma)
+% PDF for multivariate Gaussian
+% mu is broadcast in order to evaluate all m training examples from X [m
+% examples, each with n features)
+
+[m,n] = size(X);
+A = X-repmat(mu,m,1);
+p = 1/(2*pi*det(sigma)^(1/2))*exp(-0.5*sum(((A*inv(sigma)).*A)',1))';
+end
+
